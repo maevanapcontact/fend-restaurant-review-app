b3a1cd304b587cb2af9acc67c2a8045a8fcfabd4
maevanapcontact <maevanap.contact@gmail.com>
2018-06-08 11:22:18 +0200

    feat: cache assets

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@
  */
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/service-worker.js')
+    .register('./service-worker.js', {
+      scope: './'
+    })
     .then(function () {
       console.log('Service Worker registered');
     })
