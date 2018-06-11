edf18de90c54876e6e205f7e4b35bb7767c25cb4
maevanapcontact <maevanap.contact@gmail.com>
2018-06-11 12:50:47 +0200

    chore: use gulp to resize images

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,6 @@ var gulp = require('gulp');
 var jimp = require("gulp-jimp-resize");
 const image = require('gulp-image');
 
-// Copy HTML into dist
-gulp.task('copyHTML', function () {
-	gulp.src('*.html')
-		.pipe(gulp.dest('dist'))
-});
-
-// Copy CSS into dist
-gulp.task('copyCSS', function () {
-	gulp.src('css/*')
-		.pipe(gulp.dest('dist/css'))
-});
-
-// Copy data into dist
-gulp.task('copyData', function () {
-	gulp.src('data/*')
-		.pipe(gulp.dest('dist/data'))
-});
-
-// Copy JS into dist
-gulp.task('copyJS', function () {
-	gulp.src('js/*')
-		.pipe(gulp.dest('dist/js'))
-});
-
 // Resize and Optimize Images
 gulp.task('image', function () {
   gulp.src('img/*.jpg')
@@ -35,7 +11,7 @@ gulp.task('image', function () {
         ]
   	}))
     .pipe(image())
-    .pipe(gulp.dest('dist/img'));
+    .pipe(gulp.dest('img'));
 });
 
-gulp.task('default', ['copyHTML', 'copyCSS', 'copyData', 'copyJS', 'image']);
+gulp.task('default', ['image']);
